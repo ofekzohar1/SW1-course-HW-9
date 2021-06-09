530776b9413bec164564bc9175ab2553863ee019
Ofek Zohar <ofekzohar1@mail.tau.ac.il>
2021-06-10 00:51:33 +0300

    Fix for exception

--- src/il/ac/tau/cs/sw1/ex9/riddles/third/B3.java
+++ src/il/ac/tau/cs/sw1/ex9/riddles/third/B3.java
@@ -1,6 +1,8 @@
 package il.ac.tau.cs.sw1.ex9.riddles.third;
 
 public class B3 extends A3 {
+    private static final long serialVersionUID = 1L;
+
     public B3(String s) {
         super(s);
     }
