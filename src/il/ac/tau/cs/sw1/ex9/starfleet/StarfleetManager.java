945f289f55bddeea358eeadc50919f9f47ee30b6
Ofek Zohar <ofekzohar1@mail.tau.ac.il>
2021-06-12 18:37:24 +0300

    Using Stream

--- src/il/ac/tau/cs/sw1/ex9/starfleet/StarfleetManager.java
+++ src/il/ac/tau/cs/sw1/ex9/starfleet/StarfleetManager.java
@@ -1,6 +1,7 @@
 package il.ac.tau.cs.sw1.ex9.starfleet;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class StarfleetManager {
 
@@ -10,27 +11,15 @@ public class StarfleetManager {
      * name
      */
     public static List<String> getShipDescriptionsSortedByFirePowerAndCommissionYear(Collection<Spaceship> fleet) {
-        ArrayList<Spaceship> fleetList = new ArrayList<>(fleet);
-        // Sort ship list (fleet) in reversed order according to Spaceship's CompareTo method
-        fleetList.sort(Collections.reverseOrder());
-        ArrayList<String> sortedFleetAsStrings = new ArrayList<>();
-        for (Spaceship ship : fleetList) {
-            sortedFleetAsStrings.add(ship.toString()); // Add description of each ship in the fleet
-        }
-        return sortedFleetAsStrings;
+        return fleet.stream().sorted(Collections.reverseOrder()).map(Spaceship::toString).collect(Collectors.toList());
     }
 
     /**
      * Returns a map containing ship type names as keys (the class name) and the number of instances created for each type as values
      */
     public static Map<String, Integer> getInstanceNumberPerClass(Collection<Spaceship> fleet) {
-        HashMap<String, Integer> fleetCounter = new HashMap<>();
-        for (Spaceship ship : fleet) {
-            // Merge method - if key (ship) exists add 1 to it's value, if not exist add as new key with value 1
-            // Using getSimpleName() to get ship's type
-            fleetCounter.merge(ship.getClass().getSimpleName(), 1, Integer::sum);
-        }
-        return fleetCounter;
+        // x.getClass().getSimpleName() == ship type, summingInt count the type
+        return fleet.stream().collect(Collectors.groupingBy(x -> x.getClass().getSimpleName(), Collectors.summingInt(x -> 1)));
     }
 
 
@@ -38,11 +27,8 @@ public class StarfleetManager {
      * Returns the total annual maintenance cost of the fleet (which is the sum of maintenance costs of all the fleet's ships)
      */
     public static int getTotalMaintenanceCost(Collection<Spaceship> fleet) {
-        int sumMaintCost = 0;
-        for (Spaceship ship : fleet) {
-            sumMaintCost += ship.getAnnualMaintenanceCost();
-        }
-        return sumMaintCost;
+        // Sum maintenance cost
+        return fleet.stream().mapToInt(Spaceship::getAnnualMaintenanceCost).sum();
     }
 
 
@@ -50,44 +36,23 @@ public class StarfleetManager {
      * Returns a set containing the names of all the fleet's weapons installed on any ship
      */
     public static Set<String> getFleetWeaponNames(Collection<Spaceship> fleet) {
-        HashSet<String> weaponsSet = new HashSet<>();
-        for (Spaceship ship : fleet) {
-            if (ship instanceof myAbstractCombatSpaceship) { // Only if ship is a combat ship
-                myAbstractCombatSpaceship combatShip = (myAbstractCombatSpaceship) ship;
-                for (Weapon weapon : combatShip.getWeapon()) {
-                    weaponsSet.add(weapon.getName());
-                }
-            }
-        }
-        return weaponsSet;
+        return fleet.stream().filter(x -> x instanceof myAbstractCombatSpaceship).map(x -> ((myAbstractCombatSpaceship) x).getWeapon())
+                .flatMap(List::stream).map(Weapon::getName).collect(Collectors.toSet());
     }
 
     /**
      * Returns the total number of crew-members serving on board of the given fleet's ships.
      */
     public static int getTotalNumberOfFleetCrewMembers(Collection<Spaceship> fleet) {
-        int sumTotalCrewMembers = 0;
-        for (Spaceship ship : fleet) {
-            sumTotalCrewMembers += ship.getCrewMembers().size();
-        }
-        return sumTotalCrewMembers;
+        return fleet.stream().map(Spaceship::getCrewMembers).mapToInt(Set::size).sum();
     }
 
     /**
      * Returns the average age of all officers serving on board of the given fleet's ships.
      */
     public static float getAverageAgeOfFleetOfficers(Collection<Spaceship> fleet) {
-        int countTotalOfficers = 0;
-        int sumAges = 0;
-        for (Spaceship ship : fleet) {
-            for (CrewMember member : ship.getCrewMembers()) {
-                if (member instanceof Officer) { // Only if CrewMember is Officer
-                    countTotalOfficers++;
-                    sumAges += member.getAge();
-                }
-            }
-        }
-        return (float) sumAges / countTotalOfficers;
+        return fleet.stream().flatMap(x -> x.getCrewMembers().stream()).filter(x -> x instanceof Officer).map(x -> (Officer) x)
+                .collect(Collectors.averagingDouble(Officer::getAge)).floatValue();
     }
 
     /**
